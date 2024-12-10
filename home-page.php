cbff1fb07952dad06992806061c09af1650012a1
Rafael Gasparoto <rafaelgasparoto4@gmail.com>
2024-12-10 14:05:21 -0300

    Atualiza itens selecionados do carrinho
    - Busca pelo carrinho do usuário logado
    - Associa os itens do carrinho a cada carta com a quantidade selecionada

--- home-page.php
+++ home-page.php
@@ -1,14 +1,37 @@
 <?php
 require_once 'conexao.php';
+session_start();
 
+// Busca todas as cartas disponiveis
 $sql = "SELECT * FROM carta WHERE quantidade > 0";
 $result = $conn->query($sql);
-$cards = [];
-
 if ($result->num_rows > 0) {
     $cards = $result->fetch_all(MYSQLI_ASSOC);
 }
 
+// Busca itens no carrinho do usuário logado
+$cards = [];
+$itens_on_cart = [];
+$sql = "SELECT id FROM carrinho WHERE id_usuario = $_SESSION[user_id]";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $id_carrinho = $row['id'];
+    $sql = "SELECT item_carrinho.id_carta, item_carrinho.quantidade FROM item_carrinho JOIN carrinho ON item_carrinho.carrinho_id = carrinho.id WHERE carrinho.id = $id_carrinho;";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $itens_on_cart = $result->fetch_all(MYSQLI_ASSOC);
+    }
+}
+
+// Se houver itens o carrinho atualiza a quantidade selecionada de cada item
+foreach ($itens_on_cart as $item) {
+    for ($i = 0; $i < count($cards); $i++) {
+        if ($cards[$i]['id'] == $item['id_carta']) {
+            $cards[$i]['selected_quantity'] = $item['quantidade'];
+        }
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -85,7 +108,7 @@ if ($result->num_rows > 0) {
                                 <div class="mb-3">
                                     <label class="form-label">Quantidade</label>
                                     <input type="number" class="form-control"
-                                        name="quantidade" placeholder="Quantidade" min="1" max="<?= $card['quantidade'] ?>" required>
+                                        name="quantidade" placeholder="Quantidade" min="1" max="<?= $card['quantidade'] ?>" required value=<?= $card['selected_quantity'] ? $card['selected_quantity'] : 1 ?>>
                                 </div>
                                 <input type="hidden" name="id" value="<?= $card['id'] ?>">
                                 <input type="hidden" name="nome" value="<?= $card['nome'] ?>">
