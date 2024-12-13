9c8c6773cbdab3cc0742ea8d86fbeec25deeaf4e
Rafael Gasparoto <rafaelgasparoto4@gmail.com>
2024-12-12 23:04:32 -0300

    Atualiza as informações de uma carta através do id

---
+++ saller-menu/update-card.php
@@ -0,0 +1,16 @@
+<?php 
+require '../conexao.php';
+session_start();
+
+$sql = "UPDATE carta SET nome = ?, descricao = ?, tipo = ?, preco = ?, quantidade = ?, imagem_url = ? WHERE id = ?";
+
+try {
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sssdisi", $_POST['card_name'], $_POST['description'], $_POST['type'], $_POST['price'], $_POST['quantity'], $_POST['image_url'], $_POST['card_id']);
+    $stmt->execute();
+    $_SESSION['success'] = 'Carta atualizada com sucesso!';
+} catch (Exception $error) {
+    $_SESSION['error'] = 'Erro ao atualizar carta: ' . $error->getMessage();
+}
+
+header('location:add-card.php');
