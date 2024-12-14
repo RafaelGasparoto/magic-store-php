be80cc1ffc3a4731639e1a706f48d37b7ed9655c
Rafael Gasparoto <rafaelgasparoto4@gmail.com>
2024-12-14 19:05:56 -0300

    Transação finalizar pedido
    - Implementado transação na hora de finalizar um pedido
    - Evita que um pedido seja feito sem a quantidade de cartas disponível
    - Evita erro de concorrência
    - Caso houver algum problema faz rollback

--- cart/checkout-purchase.php
+++ cart/checkout-purchase.php
@@ -1,30 +1,53 @@
-<?php 
+<?php
 require 'get-cart.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     try {
         if (!is_null($itens_on_cart)) {
+            // INICIA TRANSAÇÃO PARA EVITAR VENDA DE QUANTIDADE ACIMA DO LIMITE
+            $conn->begin_transaction();
+
             $sql = "SELECT * FROM usuario WHERE id = $_SESSION[user_id]";
             $result = $conn->query($sql);
             $user = $result->fetch_assoc();
             $endereco_entrega = $user['endereco'] . ', ' . $user['cep'] . ', ' . $user['numero'] . ', ' . $user['bairro'] . ', ' . $user['cidade'] . ', ' . $user['estado'];
-            
+
             $sql = "INSERT INTO pedido (usuario_id, endereco_entrega, total, id_forma_pagamento) VALUES ($user[id], '$endereco_entrega', $total, 1)";
             $conn->query($sql);
-    
-            $sql = "SELECT id FROM pedido WHERE usuario_id = $user[id] ORDER BY id DESC LIMIT 1";
+            $pedido_id = $conn->insert_id;
+
+            // BUSCA TODAS O ID E QUANTIDADE DISPONÍVEL DE CADA CARTA INSERIDA NO CARRINHO
+            $sql = "SELECT carta.id, carta.quantidade FROM carta WHERE carta.id IN (SELECT item_carrinho.id_carta FROM item_carrinho WHERE item_carrinho.carrinho_id = $_SESSION[id_carrinho])";
             $result = $conn->query($sql);
-            $pedido_id = $result->fetch_assoc()['id'];
-    
-            foreach ($itens_on_cart as $item) {
-                $sql = "INSERT INTO item_pedido (pedido_id, carta_id, quantidade, valor) VALUES ($pedido_id, $item[id_carta], $item[quantidade], $item[preco])";
-                $conn->query($sql);
+
+            while ($row = $result->fetch_assoc()) {
+                // BUSCA O INDEX DA CARTA NO CARRINHO
+                $index = array_search($row['id'], array_column($itens_on_cart, 'id_carta'));
+
+                // VERIFICA SE A CARTA X TEM A QUANTIDADE NECESSÁRIA DISPONÍVEL
+                if ($row['quantidade'] > $itens_on_cart[$index]['quantidade']) {
+                    $item = $itens_on_cart[$index];
+                    $sql = "INSERT INTO item_pedido (pedido_id, carta_id, quantidade, valor) VALUES ($pedido_id, $item[id_carta], $item[quantidade], $item[preco])";
+                    $conn->query($sql);
+                }
             }
+
+
+            // FAZ UPDATE DE CARTAS DISPONÍVEL DE ACORDO COM A CARTA DO CARRINHO
+            $sql = "UPDATE carta 
+            JOIN item_carrinho ON carta.id = item_carrinho.id_carta
+            SET carta.quantidade = carta.quantidade - item_carrinho.quantidade
+            WHERE item_carrinho.carrinho_id = $_SESSION[id_carrinho]";
+
+            $conn->query($sql);
 
             $sql = "DELETE FROM item_carrinho WHERE carrinho_id = $_SESSION[id_carrinho]";
             $conn->query($sql);
+
+            $conn->commit();
         }
     } catch (Exception $error) {
+        $conn->rollback();
         echo $error->getMessage();
         exit();
     }
